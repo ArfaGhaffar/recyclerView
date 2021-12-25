e2470c1d4718a323b06ef36a57ea9b63c296f3a5
ArfaGhaffar <91720371+ArfaGhaffar@users.noreply.github.com>
2021-12-25 20:01:28 +0500

    Recycler view 1

--- app/src/main/java/com/example/toptrending/MainActivity.java
+++ app/src/main/java/com/example/toptrending/MainActivity.java
@@ -12,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        MyList[] myListData2 = new MyList[] {
+                new MyList("Ali Ahmed", R.drawable.bnn,"0331 203457788"),
+                new MyList("Sana Ali", R.drawable.bnn,"0331 203457788"),
+                new MyList("Asif Khan", R.drawable.bnn,"0331 203457788 "),
+                new MyList("Saba Ali", R.drawable.bnn,"0331 203457788"),
+                new MyList("Zahid Ahmed", R.drawable.bnn,"0331 203457788"),
+                new MyList("Amir Khan", R.drawable.bnn,"0331 203457788"),
+                new MyList("Raza Ahmed", R.drawable.bnn,"0331 203457788"),
+                new MyList("Hamid Ali", R.drawable.bnn,"0331 203457788"),
+
+
+        };
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        RecyclerAdapter adapter = new RecyclerAdapter(myListData2);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
     }
 }
