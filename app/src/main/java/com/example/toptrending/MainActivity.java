323db38c685f362ea3d1a180c5f9f9be6dd2d602
ArfaGhaffar <91720371+ArfaGhaffar@users.noreply.github.com>
2021-12-25 20:05:24 +0500

    Recycler view 2

--- app/src/main/java/com/example/toptrending/MainActivity.java
+++ app/src/main/java/com/example/toptrending/MainActivity.java
@@ -26,11 +26,29 @@ public class MainActivity extends AppCompatActivity {
 
         };
 
+        MyList[] myListData = new MyList[] {
+                new MyList("Email", android.R.drawable.ic_dialog_email,"Open your emails"),
+                new MyList("Info", android.R.drawable.ic_dialog_info,"Open this information"),
+                new MyList("Delete", android.R.drawable.ic_delete,"Delete this "),
+                new MyList("Dialer", android.R.drawable.ic_dialog_dialer,"Dial a number"),
+                new MyList("Alert", android.R.drawable.ic_dialog_alert,"Create an alert"),
+                new MyList("Map", android.R.drawable.ic_dialog_map,"Open your maps"),
+                new MyList("Dialer", android.R.drawable.ic_dialog_dialer,"Dial a number"),
+                new MyList("Alert", android.R.drawable.ic_dialog_alert,"Create an alert"),
+                new MyList("Map", android.R.drawable.ic_dialog_map,"Open your maps"),
+
+        };
+
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         RecyclerAdapter adapter = new RecyclerAdapter(myListData2);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
-
+        RecyclerView recyclerView2 = (RecyclerView) findViewById(R.id.recyclerView2);
+        RecyclerAdapter adapter2 = new RecyclerAdapter(myListData);
+        recyclerView2.setHasFixedSize(true);
+        recyclerView2.setLayoutManager(new LinearLayoutManager(MainActivity.this,
+                LinearLayoutManager.HORIZONTAL, false));
+        recyclerView2.setAdapter(adapter2);
     }
 }
