bbd41176b712bce8c0bebe800ae21c53a900c927
ArfaGhaffar <91720371+ArfaGhaffar@users.noreply.github.com>
2021-12-25 16:55:55 +0500

    Mylist class

--- app/src/main/java/com/example/toptrending/MyList.java
+++ app/src/main/java/com/example/toptrending/MyList.java
@@ -2,6 +2,33 @@ package com.example.toptrending;
 
 public class MyList {
 
-
+    private String description;
+    private int imgId;
+    private  String subtitle;
+    public MyList(String description, int imgId,String subtitle) {
+        this.description = description;
+        this.imgId = imgId;
+        this.subtitle=subtitle;
     }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getSubtitle() {
+        return subtitle;
+    }
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+    public int getImgId() {
+        return imgId;
+    }
+    public void setImgId(int imgId) {
+        this.imgId = imgId;
+    }
+}
+
+
 
